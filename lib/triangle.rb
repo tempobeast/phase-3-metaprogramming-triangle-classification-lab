5541f77e0b83e52cfb12cd3619a353442c1f136a
Zach <shaw.zacht@gmail.com>
2022-05-17 12:29:39 -0700

    commit tree fc5cd266a4e29cf3fb80bdf9e263a3dc12cad8b3

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_reader :kind, :sides
+
+  def initialize(first_side, second_side, third_side)
+    @sides = []
+    @sides.push(first_side, second_side, third_side)
+    @sides.sort
+
+    @first_side = first_side
+    @second_side = second_side
+    @third_side = third_side
+
+  end
+
+  def sort_sides
+    @sides.sort
+  end
+
+  def kind
+    if self.sort_sides.each {|side| side > 0} && self.sort_sides[0] + self.sort_sides[1] > self.sort_sides[2]
+      if @first_side == @second_side && @first_side == @third_side && @first_side > 0
+        :equilateral
+      elsif @first_side == @second_side || @second_side == @third_side || @third_side == @first_side
+        :isosceles
+      elsif @first_side != @second_side && @second_side != @third_side && @third_side != @first_side
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
